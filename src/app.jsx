56d7505fd0b981c908b2a87d40be25da935967bb
Bill Conley <88862631+BillConley01@users.noreply.github.com>
2021-12-20 09:10:54 -0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,54 +1,49 @@
 import React from "react";
-import LargeBackground from "./images/l-background.jpeg";
-import MediumBackground from "./images/m-background.jpeg";
-import SmallBackground from "./images/s-background.jpeg";
-
-//todo refractor component Paginate
 const Pagination = ({ items, pageSize, onPageChange }) => {
-  if (items.length <= 1) return null;
   
+  if (items.length <= 1) return null;
+
   let num = Math.ceil(items.length / pageSize);
   let pages = range(1, num);
-  
   const list = pages.map(page => {
     return (
+    
       <button key={page} 
       onClick={onPageChange} 
       className="page-item mr-1 px-2"
       style={{backgroundColor: 'lightgreen', borderColor:'lightblue'}}>
         {page}
       </button>
+
     );
   });
-
   return (
     <nav>
       <ul className="pagination ml-5">{list}</ul>
     </nav>
   );
 };
-
 const range = (start, end) => {
   return Array(end - start + 1)
     .fill(0)
     .map((item, i) => start + i);
 };
-
 function paginate(items, pageNumber, pageSize) {
   const start = (pageNumber - 1) * pageSize;
   let page = items.slice(start, start + pageSize);
   return page;
 };
-
-//custom hook
+//todo refractor component Paginate
 const useDataApi = (initialUrl, initialData) => {
+  //const { useState, useEffect, useReducer } = React;
   const [url, setUrl] = React.useState(initialUrl);
+
   const [state, dispatch] = React.useReducer(dataFetchReducer, {
     isLoading: false,
     isError: false,
     data: initialData
   });
-  
+
   React.useEffect(() => {
     let didCancel = false;
     const fetchData = async () => {
@@ -71,7 +66,6 @@ const useDataApi = (initialUrl, initialData) => {
   }, [url]);
   return [state, setUrl];
 };
-
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
     case "FETCH_INIT":
@@ -98,30 +92,14 @@ const dataFetchReducer = (state, action) => {
   }
 };
 
-//custom hook for page width state
-const handleWindowWidth = () => {
-  const [pageWidth, setPageWidth ] = React.useState(window.innerWidth);
-  
-  React.useEffect(() => {
-    const handlePageSize = () => {
-        setPageWidth(window.innerWidth);
-    }; 
-    window.addEventListener('resize', handlePageSize);
-    return () => {
-        window.removeEventListener('resize', handlePageSize);
-    }
-  }, []);
-  return pageWidth;
-};
+
 
 // App that gets data from Hacker News url
 function App() {
-  const newPageSize = handleWindowWidth()
-  const imageUrl = newPageSize >= 801 ? LargeBackground : (newPageSize >= 401) ? MediumBackground: SmallBackground;
-  
   const [query, setQuery] = React.useState("MIT");
   const [currentPage, setCurrentPage] = React.useState(1);
   const [pageSize, setPageSize] = React.useState(10);
+  
   const [{ data, isLoading, isError }, doFetch] = useDataApi(
     "https://hn.algolia.com/api/v1/search?query=MIT",
     {
@@ -131,16 +109,20 @@ function App() {
   const handlePageChange = e => {
     setCurrentPage(Number(e.target.textContent));
   };
+
   let page = data.hits;
   if (page.length >= 1) {
     page = paginate(page, currentPage, pageSize);
     console.log(`currentPage: ${currentPage}`);
   }
+
+
+
   return (
-    <div className="custom-background w-100 h-100" style={{height: '100vh', backgroundImage: `url(${imageUrl})`}}>
-      <h2 style={{color: 'lightgreen',fontWeight:'bold', textShadow: '4px 4px 5px black, 4px 4px 5px blue', marginLeft: 30, padding:20}}>Hacker News Articles</h2>
+    <div className="custom-background">
+      <h2 style={{color: 'lightgreen',fontWeight:'bold', textShadow: '4px 4px 5px black, 4px 4px 5px blue', marginLeft: 30, padding:20}}>Hacker News</h2>
     <React.Fragment>
-      <form className="d-flex ml-5"
+      <form className="ml-5"
         onSubmit={event => {
           doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
           event.preventDefault();
@@ -190,6 +172,6 @@ function App() {
   );
 }
 
-
 // ========================================
 export default App;
+
